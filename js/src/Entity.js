0a226099aab613a7be3e6ef662ab7256d8582675
demonmunchies <demonmunchies@pop-os.localdomain>
2022-02-16 17:56:00 -0500

    removed outines

--- js/src/Entity.js
+++ js/src/Entity.js
@@ -14,22 +14,12 @@ class Entity {
             this.geometries[i].position.y = this.position_y;
             this.geometries[i].position.z = this.position_z;
         }
-        for(var i = 0; i < this.outlines.length; i++) {
-            this.outlines[i].position.x = this.position_x;
-            this.outlines[i].position.y = this.position_y;
-            this.outlines[i].position.z = this.position_z;
-        }
         if(this.doesAnimate)
         {
             for(var i = 0; i < this.geometries.length; i++) {
                 this.geometries[i].rotation.x = i + Date.now() * 0.001;
                 this.geometries[i].rotation.y = i + Date.now() * 0.001;
                 this.geometries[i].rotation.z = i + Date.now() * 0.001;
-            }
-            for(var i = 0; i < this.outlines.length; i++) {
-                this.outlines[i].rotation.x = i + Date.now() * 0.001;
-                this.outlines[i].rotation.y = i + Date.now() * 0.001;
-                this.outlines[i].rotation.z = i + Date.now() * 0.001;
             }
         }
     }
@@ -52,15 +42,8 @@ class Entity {
                     _geometry.geometry.center();
                     _geometry.scale.set(10,10,10);
                     _geometry.material.side = 1;
-                    var _edges = new THREE.EdgesGeometry( _geometry.geometry );
-                    _edges.scale = 1.5;
-                    var _edge_model = new THREE.LineSegments(_edges, _geometry.material);
-                    // _edge_model.scale += 0.0
                     scene.add( _geometry );
-                    scene.add(_edge_model)
                     geometries.push(_geometry);
-                    outlines.push(_edge_model);
-                    // console.log(_geometry)
                 });
             });
         } 
