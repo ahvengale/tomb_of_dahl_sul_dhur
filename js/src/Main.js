046db96b2255aa7ce9b7aae28295b2337c008c3a
Quinn Hirt <hirtquinn@gmail.com>
2022-02-20 03:01:00 -0500

    modularized loading

--- js/src/Main.js
+++ js/src/Main.js
@@ -3,6 +3,9 @@ const renderer = new THREE.WebGLRenderer({
     canvas: document.querySelector('#C'),
 });
 
+const mtlLoader = new THREE.MTLLoader();
+const objLoader = new THREE.OBJLoader();
+
 const w = window.innerWidth;
 const h = window.innerHeight;
 const aspect_ratio = w / h;
@@ -15,29 +18,29 @@ const camera = new THREE.OrthographicCamera(
     0.1,
     1000
 );
-camera.position.set(5, 5, 5);
-camera.lookAt(scene.position);
+camera.position.set( 5, 5, 5 );
+camera.lookAt( scene.position );
 
-renderer.setSize(w, h);
+renderer.setSize( w, h );
 
-const amb_light = new THREE.AmbientLight( 0xffffff, 2.0);
-amb_light.position.set(0, 0, 0);
-scene.add(amb_light)
+const amb_light = new THREE.AmbientLight( 0xffffff, 0.2 );
+amb_light.position.set( 0, 0, 0 );
+scene.add( amb_light )
 
-const dir_light = new THREE.DirectionalLight( 0xffffff, 2.0);
-dir_light.position.set(0, 100, 50);
-scene.add(dir_light)
+const dir_light = new THREE.DirectionalLight( 0xffffff, 1.0 );
+dir_light.position.set( 0, 100, 50 );
+scene.add( dir_light )
 
-load("ModularFloor", 0, 0, 0);
-load("ModularFloor", 2, 0, 2);
-load("ModularFloor", 0, 0, 2);
-load("ModularFloor", 2, 0, 0);
+// load( "ModularFloor", 0, 0, 0 );
+// load( "ModularFloor", 2, 0, 2 );
+// load( "ModularFloor", 0, 0, 2 );
+// load( "ModularFloor", 2, 0, 0 );
 
-renderer.render(scene, camera);
+renderer.render( scene, camera );
 
 animate();
 
-function load(filename, x, y, z) {
+function load(filename, fn) {
     const mtlLoader = new THREE.MTLLoader();
     mtlLoader.setPath('models/models/obj/');
     mtlLoader.load(filename + ".mtl", function (materials) {
@@ -46,37 +49,42 @@ function load(filename, x, y, z) {
         objLoader.setMaterials(materials);
         objLoader.setPath('models/models/obj/');
         objLoader.load(filename + ".obj", function (object) {
-            object.children[0].position.set(x, y, z);
-            scene.add( object.children[0] )
+            fn(object.children[0])
         });
     });
 }
 
-const mouse = new THREE.Vector2(-1, -1);
+let temp;
+load("ModularFloor", (e) => {
+    scene.add(e);;
+})
 
-window.addEventListener('mousedown', () => {
+
+const mouse = new THREE.Vector2( -1, -1 );
+
+window.addEventListener( 'mousedown', () => {
     const raycast = new THREE.Raycaster();
-    raycast.setFromCamera(mouse, camera);
-    var intersection = raycast.intersectObjects(scene.children);
+    raycast.setFromCamera( mouse, camera );
+    var intersection = raycast.intersectObjects( scene.children );
     if (intersection.length > 0) {
-        console.log(intersection[0])
+        console.log( intersection[0] )
         intersection[0].object.visible = !intersection[0].object.visible
     }
 });
 
-document.addEventListener('mousemove', () => {
-    mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-    mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+document.addEventListener( 'mousemove', () => {
+    mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+    mouse.y = -( event.clientY / window.innerHeight ) * 2 + 1;
 });
 
-window.addEventListener('resize', () => {
+window.addEventListener( 'resize', () => {
     const new_aspect = window.innerWidth / window.innerHeight;
     camera.left = view_size * new_aspect / -2;
     camera.right = view_size * new_aspect / 2;
     camera.top = view_size / 2;
     camera.bottom = view_size / -2;
     camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setSize( window.innerWidth, window.innerHeight );
 });
 
 function animate() {
