ca8e9a48c65aea30dd793e5f11b6cfe1ee355a36
EricJoseph97 <40366704+EricJoseph97@users.noreply.github.com>
2022-02-20 20:16:07 -0500

    Modules and orbits
    Switching over to Webpack for bundling, because consistency and also make imports easier.
    
    Be sure to 'npm run build' before 'npm start'

--- App.mjs
+++ App.mjs
@@ -23,9 +23,10 @@ app.use(helmet.noSniff())
     Express middleware lets us provide all our local urls/assets with an anonymous url. 
     we will reference anything from /js as /static and anything from /res as /models even though that directory doesnt exist.
 */
-app.use('/static', express.static(path.join(__dirname, '/js'))) //virtual directory routing /static routs to local /js and sending it to client
+// app.use('/static', express.static(path.join(__dirname, '/js'))) //virtual directory routing /static routs to local /js and sending it to client
 app.use('/models', express.static(path.join(__dirname, '/res'))) //same idea with /models. This is providing all of the model objects from /res
-app.use('/node', express.static(path.join(__dirname, '/node_modules')))
+app.use('/dist', express.static(path.join(__dirname, '/dist')))
+
 
 express.static.mime.define({ 'application/javascript': ['js'] });
 express.static.mime.define({ 'text/html': ['html'] });
@@ -33,7 +34,7 @@ express.static.mime.define({ 'text/html': ['html'] });
 app.get('/', (req, res) => {
     res.setHeader('Content-Type', 'text/html')
     //__dirname is our dynamic root path
-    res.sendFile(path.join(__dirname, 'index.html'))
+    res.sendFile(path.join(__dirname, 'dist/index.html'))
 })
 
 app.listen(port, () => {
